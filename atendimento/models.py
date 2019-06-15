708c0a141a16ec2a5577e1b3fc98a9d5adfea31b
Misael B. de Queiroz <misael.queiroz@ifrn.edu.br>
2019-06-15 20:40:16 -0300

    * Criando um primeiro modelo.

--- atendimento/models.py
+++ atendimento/models.py
@@ -1,3 +1,26 @@
 from django.db import models
 
 # Create your models here.
+from django.db import models
+
+
+class Cliente(models.Model):
+    ESTADO_CIVIL_CHOICES = (
+        ('SOLTEIRO', u'Solteiro'),
+        ('CASADO', u'Casado'),
+        ('DIVORCIADO', u'Divorciado'),
+        ('VIUV', u'Viúvo'),
+    )
+
+    nome = models.CharField(verbose_name='Nome', max_length=100)
+    estado_civil = models.CharField(verbose_name='Estado Civil', max_length=10, choices=ESTADO_CIVIL_CHOICES)
+    cpf = models.CharField(verbose_name='CPF', max_length=14)
+    data_nascimento = models.DateField(blank=True, null=True)
+    email = models.EmailField(max_length=100, blank=True, null=True)
+
+    class Meta:
+        ordering = ['nome']
+
+    # Label padrão que será exibido em toda o admin do Django.
+    def __str__(self):
+        return self.nome
