ddeed29f6f76f624515dd854abb7ec00267e907f
Misael B. de Queiroz <misael.queiroz@ifrn.edu.br>
2019-06-15 19:14:51 -0300

    * Adicionando exemplo simples de como utilizar um template em arquivo no Django.

--- atendimento/urls.py
+++ atendimento/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path('index', views.index, name='index'),
     path('index2', views.index2, name='index2'),
     path('index3', views.index3, name='index3'),
+    path('index4', views.index4, name='index4'),
 ]
--- atendimento/views.py
+++ atendimento/views.py
@@ -1,6 +1,7 @@
-from django.http import HttpResponse
 import datetime
-from django.template import Template, Context
+
+from django.http import HttpResponse
+from django.template import Template, Context, loader
 
 
 # Create your views here.
@@ -21,7 +22,17 @@ def index3(request):
     # Obs: A data já é exibida de acordo com a configuração realizada no
     # Django: It timeeeeeee 3 de Julho de 2014 às 11:59.
     template = Template('<html><body>Houston, now is {{ current_date }}.</body></html>')
+    # Contexto representa o conjunto de variáveis que serão processadas pelo template.
     context = Context({'current_date': now})
     html = template.render(context)
 
     return HttpResponse(html)
+
+# Exemplo de controle que retorna como resposta um html simples através de um template armazenado em arquivo.
+def index4(request):
+    # Obs: Para o loader funcionar, se faz necessário que a aplicação esteja listada no settings.INSTALLED_APPS.
+    template = loader.get_template('atendimento/index4.html')
+    context = {
+        'current_date': datetime.datetime.now(),
+    }
+    return HttpResponse(template.render(context, request))
