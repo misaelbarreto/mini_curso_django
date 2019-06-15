1e005d1cdd0c3569d7f409c1900395af417c6911
Misael B. de Queiroz <misael.queiroz@ifrn.edu.br>
2019-06-15 18:32:20 -0300

    * Adicionando exemplo simples de como utilizar o template do Django.

--- atendimento/urls.py
+++ atendimento/urls.py
@@ -4,4 +4,6 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('index2', views.index2, name='index2'),
+    path('index3', views.index3, name='index3'),
 ]
--- atendimento/views.py
+++ atendimento/views.py
@@ -1,7 +1,27 @@
-from django.shortcuts import render
 from django.http import HttpResponse
+import datetime
+from django.template import Template, Context
+
+
 # Create your views here.
 
-
+# Exemplo de controle que retorna como resposta um texto simples.
 def index(request):
-    return HttpResponse("Hi Houston! It's all right!")
+    return HttpResponse('Hi Houston! It s all right!')
+
+# Exemplo de controle que retorna como resposta um html simples.
+def index2(request):
+    html = '<html><body style="color: red">Hi Houston! It s all right!</body></html>'
+    return HttpResponse(html)
+
+# Exemplo de controle que retorna como resposta um html simples através de um template criado em tempo de execução.
+def index3(request):
+    now = datetime.datetime.now()
+
+    # Obs: A data já é exibida de acordo com a configuração realizada no
+    # Django: It timeeeeeee 3 de Julho de 2014 às 11:59.
+    template = Template('<html><body>It timeeeeeee {{ current_date }}.</body></html>')
+    context = Context({'current_date': now})
+    html = template.render(context)
+
+    return HttpResponse(html)
