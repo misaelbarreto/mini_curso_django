2967435cb03da5ac26684cc56b443a8e3589df2a
Misael B. de Queiroz <misael.queiroz@ifrn.edu.br>
2019-06-15 21:17:28 -0300

    * Mostrando customizações que dá pra fazer via Admin.

--- atendimento/admin.py
+++ atendimento/admin.py
@@ -3,4 +3,16 @@ from .models import Cliente
 
 # Register your models here.
 
-admin.site.register(Cliente)
+# Exemplo 1: Registrar um modelo junto ao Django Admin.
+# admin.site.register(Cliente)
+
+
+# Exemplo 2: Registrar um modelo junto ao Django Admin e fazendo customizações básicas.
+class ClienteAdmin(admin.ModelAdmin):
+    fields = ['nome', 'cpf', 'estado_civil',]
+    search_fields = ['nome', 'cpf']
+    list_filter = ['estado_civil', ]
+    list_display = ('nome', 'cpf', 'estado_civil', 'data_nascimento', 'email')
+    list_display_links = ('nome',)
+
+admin.site.register(Cliente, ClienteAdmin)
