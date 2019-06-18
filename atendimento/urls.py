bb8de7db12867fbe6c7378953ecd1bf99c8012d9
Misael B. de Queiroz <misael.queiroz@ifrn.edu.br>
2019-06-18 09:46:56 -0300

    * Correção de urls.

--- atendimento/urls.py
+++ atendimento/urls.py
@@ -8,6 +8,7 @@ urlpatterns = [
     path('index2', views.index2, name='index2'),
     path('index3', views.index3, name='index3'),
     path('index4a', views.index4a, name='index4a'),
-    path('index4b', views.index4b, name='index4a'),
-    path('index4b/<int:idade>', views.index4b, name='index4a'),
+    path('index4b', views.index4b, name='index4b'),
+    path('index4b/<int:idade>', views.index4b, name='index4b'),
 ]
+
